814c8df1d863d77e4374b9aa882f094ebe67e8b6
Cookies <chnvcode@gmail.com>
2015-03-24 10:39:39 +0800

    Rename files, add image

--- reload.sh
+++ reload.sh
@@ -3,21 +3,21 @@
 ## php-fpm
 
 sudo launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.php*.plist
-sudo launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.php*.plist
+sudo launchctl load -wF ~/Library/LaunchAgents/homebrew.mxcl.php*.plist
 
 echo "php-fpm restarted"
 
 ##  mysql
 
 sudo launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
-sudo launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+sudo launchctl load -wF ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
 
 echo "mysql restarted"
 
 ## nginx
 
 sudo launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist
-sudo launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist
+sudo launchctl load -wF ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist
 
 echo "nginx restarted"
 
