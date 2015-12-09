b0ebf648983ec545a91c9cb1044935f74875a26a
chnvcode <chnvcode@gmail.com>
2015-12-09 12:56:51 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,39 +1,12 @@
 #!/bin/sh
 
-if [ $# -gt 0 ]; then
-    pname="$1"
-else
-    pname="all"
-fi
+command -v brew >/dev/null 2>&1 || { 
+    echo >&2 "Homebrew is required but not installed. Aborting."; 
+    exit 0;
+}
 
-if [ $pname = "php-fpm" -o $pname = "all" ]; then
-    echo "Restarting php-fpm..."
-    sudo launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.php*.plist
-    sudo launchctl load -wF ~/Library/LaunchAgents/homebrew.mxcl.php*.plist
-fi
+brew tap homebrew/services >/dev/null 2>&1
 
-if [ $pname = "mysql" -o $pname = "all" ]; then
-    echo "Restarting mysql..."
-    sudo launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
-    sudo launchctl load -wF ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
-fi
-
-if [ $pname = "nginx" -o $pname = "all" ]; then
-    echo "Restarting nginx..."
-    sudo launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist
-    sudo launchctl load -wF ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist
-fi
-
-if [ $pname = "memcached" -o $pname = "all" ]; then
-    echo "Restarting memcached..."
-    sudo launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.memcached.plist
-    sudo launchctl load ~/Library/LaunchAgents/homebrew.mxcl.memcached.plist
-fi
-
-if [ $pname = "dnsmasq" -o $pname = "all" ]; then
-    echo "Restarting dnsmasq..."
-    sudo launchctl stop homebrew.mxcl.dnsmasq
-    sudo launchctl start homebrew.mxcl.dnsmasq
-fi
+echo "This script is deprecated. Please use command '$ brew services' instead."
 
 exit 0
